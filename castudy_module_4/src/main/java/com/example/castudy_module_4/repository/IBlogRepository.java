0b507b0e7c26b9c0a34857b217edb9c2ff6a6591
nvhoang291299 <nvhoang020100@gmail.com>
2023-05-23 16:28:45 +0700

    update1

--- castudy_module_4/src/main/java/com/example/castudy_module_4/repository/IBlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.castudy_module_4.repository;
-
-import com.example.castudy_module_4.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBlogRepository implements JpaRepository<Blog, int> {
-}
