b03d4cb4c75a3a473d26d37a707abf529328d2ca
AnhDao19112001 <phantaanhdao@gmail.com>
2023-05-25 10:08:17 +0700

    update product

--- castudy_module_4/src/main/java/com/example/castudy_module_4/service/IEmployeeTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.castudy_module_4.service;
-
-import com.example.castudy_module_4.model.employee.EmployeeType;
-
-import java.util.List;
-
-public interface IEmployeeTypeService {
-    List<EmployeeType> getAll();
-}
